a63beb4797fb5d520b779ddeec0774d9a4b57e88
Phoenix Pulsar <phoenixpulsar+github@protonmail.com>
2022-04-01 15:56:39 -0500

    Step 3.2 Added 3 main models and some imports from near-sdk-as

---
+++ src/vehicleGarage/assembly/models.ts
@@ -0,0 +1,8 @@
+import { context, storage, PersistentMap, PersistentSet } from "near-sdk-as";
+
+@nearBindgen
+export class Vehicle {}
+@nearBindgen
+export class VehicleService {}
+@nearBindgen
+export class VehicleGarage {}
