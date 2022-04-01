8833514b8d39f2a52eeaa86830afdbf7db2e081e
Phoenix Pulsar <phoenixpulsar+github@protonmail.com>
2022-04-01 16:10:19 -0500

    Step 4.3 Connected/link functions with models

--- src/vehicleGarage/assembly/index.ts
+++ src/vehicleGarage/assembly/index.ts
@@ -1,11 +1,73 @@
 import { VehicleId, VehicleServiceId, AccountId } from "../../utils";
 import { VehicleGarage } from "../models";
 
-export function init(): void {}
-export function get_vehicle_garage(): VehicleGarage {}
-export function add_vehicle(): void {}
-export function update_vehicle(): void {}
-export function delete_vehicle(): void {}
-export function add_vehicle_service(): void {}
-export function update_vehicle_service(): void {}
-export function delete_vehicle_service(): void {}
+export function init(): void {
+  VehicleGarage.create_garage();
+}
+export function get_vehicle_garage(): VehicleGarage {
+  return VehicleGarage.get_garage();
+}
+export function add_vehicle(
+  year: string,
+  make: string,
+  model: string,
+  owner: AccountId,
+  vehicleNotes: string,
+  dateAcquired: string
+): void {
+  VehicleGarage.add_vehicle(
+    year,
+    make,
+    model,
+    owner,
+    vehicleNotes,
+    dateAcquired
+  );
+}
+export function update_vehicle(
+  vehicleId: VehicleId,
+  year: string,
+  make: string,
+  model: string,
+  owner: AccountId,
+  vehicleNotes: string,
+  dateAcquired: string
+): void {
+  VehicleGarage.update_vehicle(
+    vehicleId,
+    year,
+    make,
+    model,
+    owner,
+    vehicleNotes,
+    dateAcquired
+  );
+}
+export function delete_vehicle(vehicleId: VehicleId): void {
+  VehicleGarage.delete_vehicle(vehicleId);
+}
+export function add_vehicle_service(
+  vehicleId: VehicleId,
+  serviceDate: string,
+  serviceNotes: string
+): void {
+  VehicleGarage.add_vehicle_service(vehicleId, serviceDate, serviceNotes);
+}
+export function update_vehicle_service(
+  vehicleServiceId: VehicleServiceId,
+  vehicleId: VehicleId,
+  serviceDate: string,
+  serviceNotes: string
+): void {
+  VehicleGarage.update_vehicle_service(
+    vehicleServiceId,
+    vehicleId,
+    serviceDate,
+    serviceNotes
+  );
+}
+export function delete_vehicle_service(
+  vehicleServiceId: VehicleServiceId
+): void {
+  VehicleGarage.delete_vehicle_service(vehicleServiceId);
+}
