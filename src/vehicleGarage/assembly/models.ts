2e5230897b143a9e6fcf94d5e4705ed9f0a63a7b
Phoenix Pulsar <phoenixpulsar+github@protonmail.com>
2022-04-01 16:01:50 -0500

    Step 3.5 Implemented Vehicle and VehicleService class/model

--- src/vehicleGarage/assembly/models.ts
+++ src/vehicleGarage/assembly/models.ts
@@ -9,9 +9,35 @@ import {
 } from "../../utils";
 
 @nearBindgen
-export class Vehicle {}
+export class Vehicle {
+  public id: VehicleId = idCreator();
+  created_at: Timestamp = context.blockTimestamp;
+  public serviceIds: PersistentSet<VehicleServiceId> = new PersistentSet<VehicleServiceId>(
+    "s"
+  );
+
+  constructor(
+    public year: string,
+    public make: string,
+    public model: string,
+    public owner: AccountId,
+    public vehicleNotes: string,
+    public dateAcquired: string
+  ) {}
+}
+
 @nearBindgen
-export class VehicleService {}
+export class VehicleService {
+  public id: VehicleServiceId = idCreator();
+  created_at: Timestamp = context.blockTimestamp;
+
+  constructor(
+    public vehicleId: VehicleId,
+    public serviceDate: string,
+    public serviceNotes: string
+  ) {}
+}
+
 @nearBindgen
 export class VehicleGarage {}
 
